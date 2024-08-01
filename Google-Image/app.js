96af8ec5273535d237ced2670c2259012d9fc710
Bhavik-Sanghar <130123723+Bhavik-Sanghar@users.noreply.github.com>
2024-08-01 10:22:08 +0530

    Update model

--- Google-Image/app.js
+++ Google-Image/app.js
@@ -20,7 +20,7 @@ const apiKey = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-pro-vision",
+  model: "gemini-1.5-flash",
 });
 
 const safetySettings = [
--- Node-Email/index.js
+++ Node-Email/index.js
@@ -1,26 +1,38 @@
+// Import the dotenv library to load environment variables from a .env file
 require('dotenv').config();
-    const nodemailer = require('nodemailer');
 
-    const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    }
-  });
+// Import the nodemailer library for sending emails
+const nodemailer = require('nodemailer');
 
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL_TO,
-    subject: 'Sending Email using Node.js - Improved',
-    text: 'Test ENV file 💌😎 Improved'
-  };
+// Create a transporter object using the Gmail service
+// This object will be used to send emails
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    // Use the EMAIL and PASSWORD environment variables for authentication
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+// Define the email options
+// This object contains the sender, recipient, subject, and body of the email
+const mailOptions = {
+  from: process.env.EMAIL, // Sender's email address
+  to: process.env.EMAIL_TO, // Recipient's email address
+  subject: 'Sending Email using Node.js - Improved', // Email subject
+  text: 'Test ENV file 💌😎 Improved' // Email body
+};
+
+// Send the email using the transporter object and the mail options
+// The callback function handles the response from the email server
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    // If there is an error, log it to the console
+    console.log(error);
+  } else {
+    // If the email is sent successfully, log the response to the console
+    console.log('Email sent: ' + info.response);
+  }
+});
 
